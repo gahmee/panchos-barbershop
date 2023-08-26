d6fc1bd327e434e51fc7323d2ca9d8bec53f7158
Gamy Aguirre <gamyag@gmail.com>
2023-08-25 19:56:14 -0600

    landing section styling

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,12 +3,24 @@ import { Link } from "react-router-dom"
 const Navbar = () => {
     return (
         <div className="navbar">
-            <Link to="/" ><h1>Panchos Barbershop</h1></Link>
-            <div>
+            <div id="nav-buttons">
+                <div>Services</div>
+                -
+                <div>Barbers</div>
+                -
+                <div>Products</div>
+            </div>
+            <div id="home-button">
+                <Link to="/" ><h1>Pancho's</h1></Link>
+            </div>
+            <Link id="book-appointment-button">
+                BOOK NOW
+            </Link>
+            {/* <div>
                 <Link to="/login" >Login</Link>
                 <Link to="/signup" >Sign Up</Link>
                 <Link to="/bookappointment" >Book Appointment</Link>
-            </div>
+            </div> */}
         </div>
     )
 }
--- frontend/src/pages/BookAppointment.js
+++ frontend/src/pages/BookAppointment.js
@@ -1,9 +1,35 @@
-
+import { useState } from 'react'
 const BookAppointment = () => {
+    const [barber, setBarber] = useState()
+    const [service, setService] = useState()
+    const [date, setDate] = useState()
+    const [time, setTime] = useState()
+    const [name, setName] = useState()
+    const [phone, setPhone] = useState()
+
+
     const today = new Date()
-    today.setDate(today.getDate() - 1)
     const threeWeeksOut = new Date()
     threeWeeksOut.setDate(today.getDate() + 21)
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const appointment = { barber, date, service, name }
+
+
+        const response = await fetch('http://localhost:4000/api/appointments', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(appointment)
+        })
+
+
+        console.log(response)
+
+        const json = await response.json()
+
+    }
 
 
     return (
@@ -14,7 +40,7 @@ const BookAppointment = () => {
                     <form>
                         <label>
                             Barber:
-                            <select>
+                            <select onChange={(e) => setBarber(e.target.value)}>
                                 <option>Barber A</option>
                                 <option>Barber B</option>
                                 <option>Barber C</option>
@@ -22,12 +48,12 @@ const BookAppointment = () => {
                         </label>
                         <label>
                             Service:
-                            <select>
+                            <select onChange={(e) => setService(e.target.value)}>
                                 <option>Haircut / Beard Trim</option>
                                 <option>Shave</option>
                             </select>
                         </label>
-                        <label>
+                        <label onChange={(e) => setDate(e.target.value)}>
                             Date:
                             <input type="date" min={today.toISOString().split('T')[0]} max={threeWeeksOut.toISOString().split('T')[0]}></input>
                         </label>
@@ -37,12 +63,12 @@ const BookAppointment = () => {
                         </label>
                         <label>
                             Name:
-                            <input></input>
+                            <input type="text" onChange={(e) => setName(e.target.value)}></input>
                         </label><label>
                             Phone:
-                            <input></input>
+                            <input type="tel" onChange={(e) => setBarber(e.target.value)}></input>
                         </label>
-                        <button>Submit</button>
+                        <button onClick={(e) => handleSubmit(e)}>Submit</button>
                     </form>
                 </div>
             </div>
