386a7783c5941455dad295d3e296f50956242e34
Gamy Aguirre <gamyag@gmail.com>
2023-09-05 18:22:05 -0600

    remove backend/media resize fix

--- frontend/src/components/home/Footer.js
+++ frontend/src/components/home/Footer.js
@@ -1,8 +1,41 @@
 const Footer = () => {
     return (
         <div id="footer">
-
-            Footer
+            <div id="social-container">TWITTER FACEBOOK INSTAGRAM THREADS</div>
+            <div id="business-hours-container">
+                <table id="days-container">
+                    <tr>
+                        <td>SUNDAY</td>
+                        <td>CLOSED</td>
+                    </tr>
+                    <tr>
+                        <td>MONDAY</td>
+                        <td>9AM - 5PM</td>
+                    </tr>
+                    <tr>
+                        <td>TUESDAY</td>
+                        <td>9AM - 5PM</td>
+                    </tr>
+                    <tr>
+                        <td>WEDNESDAY</td>
+                        <td>9AM - 5PM</td>
+                    </tr>
+                    <tr>
+                        <td>THURSDAY</td>
+                        <td>9AM - 5PM</td>
+                    </tr>
+                    <tr>
+                        <td>FRIDAY</td>
+                        <td>9AM - 5PM</td>
+                    </tr>
+                    <tr>
+                        <td>SATURDAY</td>
+                        <td>9AM - 5PM</td>
+                    </tr>
+                </table>
+            </div>
+            <div id="location-container">421 Thomas Rd.
+                Mahopac, NY 10541</div>
 
         </div>
     )
--- frontend/src/components/home/Products.js
+++ frontend/src/components/home/Products.js
@@ -1,6 +1,7 @@
 import blacktop from "../../images/products/blacktop-pomade-6oz-front.jpg"
 import classic from "../../images/products/classic-pomade-6oz-front.jpg"
 import fiber from "../../images/products/fiber-pomade-6oz-front.jpg"
+import gel from "../../images/products/gel-pomade.jpg"
 import imperialLogo from "../../images/products/imperial-logo.jpg"
 
 const Products = () => {
@@ -8,11 +9,13 @@ const Products = () => {
         <div id="products-section">
             <div id="product-section-title">
                 <img src={imperialLogo}></img>
+                SOLD HERE
             </div>
             <div id="product-container">
                 <img className="product-picture" src={blacktop}></img>
                 <img className="product-picture" src={classic}></img>
                 <img className="product-picture" src={fiber}></img>
+                <img className="product-picture" src={gel}></img>
             </div>
 
         </div>
